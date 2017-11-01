dab88220393046e634df04932ed290cc9d7ead8d
Oday Rafeh <Udayrafehhhhh@gmail.com>
2017-11-01 14:56:42 +0100

    the final simple code

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+        
+        <meta charset="UTF-8">
+        
+        <title>Battleship</title>
+
+        <link rel="stylesheet" type="text/css" href="BattelshipCSS.css">
+                
+</head>
+
+    <body>
+
+        <center>
+                <h1>Battleship</h1>	
+
+                <p>Click to shoot at a ship!</p>
+
+                <div id="gameboard"> </div>
+
+	
+	<script type="text/javascript" src="Battelship java script code.js"></script>
+    </body>
+        </center>
+</html>
